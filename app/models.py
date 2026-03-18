cf96b044bb9828167094711ab25f573ed12ecb52
Simatwa <smartwacaleb@gmail.com>
2026-03-18 13:48:14 +0300

    feat: add js-runtime environment to ytdl options

--- app/models.py
+++ app/models.py
@@ -1,17 +1,18 @@
 """Global models"""
 
+import logging
+import os
+from pathlib import Path
+from typing import Literal
+
 from pydantic import (
     BaseModel,
-    Field,
-    field_validator,
-    PositiveInt,
     EmailStr,
+    Field,
     HttpUrl,
+    PositiveInt,
+    field_validator,
 )
-from typing import Optional, Literal
-from pathlib import Path
-import os
-import logging
 
 
 class CustomWebsocketResponse(BaseModel):
@@ -21,108 +22,117 @@ class CustomWebsocketResponse(BaseModel):
 
 class EnvVariables(BaseModel):
     # contacts
-    contact_name: Optional[str] = "Unknown"
-    contact_email: Optional[EmailStr] = "email@localhost.dev"
-    contact_url: Optional[HttpUrl] = "http://localhost:8000/"
+    contact_name: str | None = "Unknown"
+    contact_email: EmailStr | None = "email@localhost.dev"
+    contact_url: HttpUrl | None = "http://localhost:8000/"
     # API
-    api_description: Optional[str] = ""
-    api_title: Optional[str] = "Youtube-Downloader"
-    api_description: Optional[str] = ""
-    api_terms_of_service: Optional[HttpUrl] = "http://localhost:8000/terms-of-service"
+    api_description: str | None = ""
+    api_title: str | None = "Youtube-Downloader"
+    api_terms_of_service: HttpUrl | None = "http://localhost:8000/terms-of-service"
 
-    visitorData: Optional[str] = Field(
+    visitorData: str | None = Field(
         None, description="Extracted along with po token"
     )
-    po_token: Optional[str] = Field(
+    po_token: str | None = Field(
         None,
         description="How to extract it refer to : https://github.com/yt-dlp/yt-dlp/wiki/Extractors#"
         "manually-acquiring-a-po-token-from-a-browser-for-use-when-logged-out",
     )
-    filename_prefix: Optional[str] = ""
-    working_directory: Optional[str] = os.getcwd()
-    clear_temps: Optional[bool] = True
-    search_limit: Optional[int] = 50
-    video_info_cache_period_in_hrs: Optional[PositiveInt] = 4
-    database_engine: Optional[str] = "sqlite:///db.sqlite3"
+    filename_prefix: str | None = ""
+    working_directory: str | None = os.getcwd()
+    clear_temps: bool | None = True
+    search_limit: int | None = 50
+    video_info_cache_period_in_hrs: PositiveInt | None = 4
+    database_engine: str | None = "sqlite:///db.sqlite3"
     default_extension: Literal["mp4", "webm"] = "webm"
-    frontend_dir: Optional[str] = None
+    frontend_dir: str | None = None
 
     # static server options
-    static_server_url: Optional[str] = None
+    static_server_url: str | None = None
 
-    serve_frontend_from_static_server: Optional[bool] = False
+    serve_frontend_from_static_server: bool | None = False
 
-    api_base_url: Optional[str] = None
+    api_base_url: str | None = None
 
     # Downloader params - yt_dlp
     default_audio_format: Literal["webm", "m4a"] = "m4a"
-    enable_logging: Optional[bool] = False
-    proxy: Optional[str] = None
-    cookiefile: Optional[str] = None
-    http_chunk_size: Optional[int] = 4096
-    updatetime: Optional[bool] = False
-    buffersize: Optional[int] = None
-    ratelimit: Optional[int] = None
-    throttledratelimit: Optional[int] = None
-    min_filesize: Optional[int] = None
-    max_filesize: Optional[int] = None
-    noresizebuffer: Optional[bool] = None
-    retries: Optional[int] = 2
-    continuedl: Optional[bool] = False
-    noprogress: Optional[bool] = False
-    nopart: Optional[bool] = False
-    concurrent_fragment_downloads: Optional[int] = 1
+    enable_logging: bool | None = False
+    proxy: str | None = None
+    cookiefile: str | None = None
+    http_chunk_size: int | None = 4096
+    updatetime: bool | None = False
+    buffersize: int | None = None
+    ratelimit: int | None = None
+    throttledratelimit: int | None = None
+    min_filesize: int | None = None
+    max_filesize: int | None = None
+    noresizebuffer: bool | None = None
+    retries: int | None = 2
+    continuedl: bool | None = False
+    noprogress: bool | None = False
+    nopart: bool | None = False
+    concurrent_fragment_downloads: int | None = 1
     # YoutubeDL params
-    verbose: Optional[bool] = None
-    quiet: Optional[bool] = None
-    allow_multiple_video_streams: Optional[bool] = None
-    allow_multiple_audio_streams: Optional[bool] = None
-    geo_bypass: Optional[bool] = True
-    geo_bypass_country: Optional[str] = None
+    verbose: bool | None = None
+    quiet: bool | None = None
+    allow_multiple_video_streams: bool | None = None
+    allow_multiple_audio_streams: bool | None = None
+    geo_bypass: bool | None = True
+    geo_bypass_country: str | None = None
     # Post-download opts
-    embed_subtitles: Optional[bool] = False
+    embed_subtitles: bool | None = False
 
     append_id_in_filename: bool = False
+
+    js_runtimes: str | None = None
 
     @property
     def ytdlp_params(self) -> dict[str, int | bool | None]:
 
         if self.serve_frontend_from_static_server and not self.frontend_dir:
-            raise Exception(
+            raise Exception(  # noqa: TRY002
                 "You have specified to serve frontend contents from static server "
                 "yet you have NOT specified the FRONTEND-DIR. "
                 "Set the path to frontend_dir in the .env (config) file."
             )
-        params = dict(
-            cookiefile=self.cookiefile,
+
+        params = {
+            "cookiefile": self.cookiefile,
             # http_chunk_size=self.http_chunk_size, # activating this makes
             # download speed so slow. Consider giving it a fix.
-            updatetime=self.updatetime,
+            "updatetime": self.updatetime,
             # buffersize=self.buffersize,
-            ratelimit=self.ratelimit,
-            throttledratelimit=self.throttledratelimit,
-            min_filesize=self.min_filesize,
-            max_filesize=self.max_filesize,
-            noresizebuffer=self.noresizebuffer,
-            retries=self.retries,
-            continuedl=self.continuedl,
-            noprogress=self.noprogress,
-            nopart=self.nopart,
-            concurrent_fragment_downloads=self.concurrent_fragment_downloads,
-            verbose=self.verbose,
-            quiet=self.quiet,
-            allow_multiple_video_streams=self.allow_multiple_video_streams,
-            allow_multiple_audio_streams=self.allow_multiple_audio_streams,
-            geo_bypass=self.geo_bypass,
-            geo_bypass_country=self.geo_bypass_country,
-            keep_fragments=False,
-            fragment_retries=2,
-        )
+            "ratelimit": self.ratelimit,
+            "throttledratelimit": self.throttledratelimit,
+            "min_filesize": self.min_filesize,
+            "max_filesize": self.max_filesize,
+            "noresizebuffer": self.noresizebuffer,
+            "retries": self.retries,
+            "continuedl": self.continuedl,
+            "noprogress": self.noprogress,
+            "nopart": self.nopart,
+            "concurrent_fragment_downloads": self.concurrent_fragment_downloads,
+            "verbose": self.verbose,
+            "quiet": self.quiet,
+            "allow_multiple_video_streams": self.allow_multiple_video_streams,
+            "allow_multiple_audio_streams": self.allow_multiple_audio_streams,
+            "geo_bypass": self.geo_bypass,
+            "geo_bypass_country": self.geo_bypass_country,
+            "keep_fragments": False,
+            "fragment_retries": 2,
+            # runtimes
+        }
+
+        if self.js_runtimes:
+            params["js_runtimes"] = self.js_runtimes
+
         if self.proxy:
             # Passing proxy with null value makes download to fail
             params["proxy"] = self.proxy
+
         if self.enable_logging:
             params["logger"] = logging.getLogger("uvicorn")
+
         if self.quiet:
             # Assumes it's running in production mode
             logging.getLogger("yt_dlp_bonus").setLevel(logging.ERROR)
@@ -185,6 +195,15 @@ class EnvVariables(BaseModel):
             raise TypeError(f"Invalid cookiefile passed - {value}")
         return value
 
+    @field_validator("js_runtimes")
+    def validate_js_runtimes(value):
+        if not value:
+            return
+        file = Path(value)
+        if not file.exists() or not file.is_file():
+            raise TypeError(f"Invalid valuse for js_runtime passed - {value}")
+        return value
+
     @field_validator("frontend_dir")
     def validate_frontend_dir(value):
         if not value:
@@ -214,11 +233,11 @@ class EnvVariables(BaseModel):
     @property
     def contacts(self) -> dict[str, str | EmailStr]:
         """API' contact details"""
-        return dict(
-            name=self.contact_name,
-            email=str(self.contact_email),
-            url=str(self.contact_url),
-        )
+        return {
+            "name": self.contact_name,
+            "email": str(self.contact_email),
+            "url": str(self.contact_url),
+        }
 
     @property
     def api_base_url_validated(self) -> str:
